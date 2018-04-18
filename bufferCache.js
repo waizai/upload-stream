85e5027003db4e11eb795b036fb25759661fb701
dangxiaoli <dangxiaoli@meituan.com>
2018-04-18 20:56:55 +0800

    first commit

--- bufferCache.js
+++ bufferCache.js
@@ -2,14 +2,57 @@
 * @Author: dangxiaoli
 * @Date:   2018-04-17 20:10:33
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2018-04-17 20:23:22
+* @Last Modified time: 2018-04-18 17:15:54
 */
 class BufferCache {
     constructor(cutSize = 2097152){
         //Allocates a new Buffer of size bytes
         this._cache = Buffer.alloc(0);
         this.cutSize = cutSize;
-
+        //cache list
         this.readyCache = [];
     }
+
+    // 放入不同大小的buffer
+    pushBuf(buf){
+        let cacheLength = this._cache.length;
+        let bufLength = buf.length;
+
+        this._cache = Buffer.concat([this._cache, buf], cacheLength + bufLength);
+
+        this.cut();
+    }
+
+    //切分成等份的
+    cut(){
+        if(this._cache.length >= this.cutSize){
+            let totalLen = this._cache.length;
+            let cutCount = Math.floor(totalLen / this.cutSize);
+
+            for(let i = 0; i < cutCount; i++){
+                let newBuf = Buffer.alloc(this.cutSize);
+                this._cache.copy(newBuf, 0, i * this.cutSize, (i + 1) * this.cutSize);
+                this.readyCache.push(newBuf);
+            }
+
+            this._cache = this._cache.slice(cutCount * this.cutSize);
+        }
+    }
+
+    getChunks(){
+        return this.readyCache;
+    }
+
+    //获取数据包的最后一小节
+    getRemainChunks(){
+        if(this._cache.length <= this.cutSize){
+            return this._cache;
+        }else{
+            this.cut();
+            return this.getRemainChunks();
+        }
+    }
 }
+
+
+module.exports = BufferCache;
--- index.js
+++ index.js
@@ -2,8 +2,61 @@
 * @Author: dangxiaoli
 * @Date:   2018-04-17 19:52:02
 * @Last Modified by:   dangxiaoli
-* @Last Modified time: 2018-04-17 19:52:22
+* @Last Modified time: 2018-04-18 18:04:12
 */
 let request = require('request');
 let fs = require('fs');
+let BufferCache = require('./bufferCache');
+const chunkSplice = 2097152; // 2MB
+let bufferCache = new BufferCache(chunkSplice);
+
+
+function getChunks(url, onStartDownload, onDownloading, onDownloadClose){
+    let totalLength = 0;
+
+    let httpStream = request({
+        method: 'GET',
+        url: url
+    });
+
+    let writeStream = fs.createWriteStream('/dev/null');
+
+    httpStream.pipe(writeStream);
+
+    httpStream.on('response',(response) => {
+        onStartDownload(response.headers)
+    }).on('data',(chunk) => {
+        totalLength += chunk.length;
+        console.log(`${totalLength} : ${chunk}`);
+        onDownloading(chunk, totalLength);
+    });
+
+    httpStream.on('close',() => {
+        console.log('download finished');
+        onDownloadClose(totalLength);
+    });
+
+}
+
+function onStart(headers){
+    console.log(`start downloading, headers is :${headers}`);
+}
+
+function onData(chunk, downloadedLength){
+    console.log(`write${chunk.length}KB into cache`);
+    bufferCache.pushBuf(chunk);
+}
+
+function onFinished(totalLength){
+    let chunkCount = Math.ceil()
+}
+
+
+
+
+
+
+
+
+
 
